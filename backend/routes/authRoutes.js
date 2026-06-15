e49e4540c8dd8902a813f333ab8c5763fac4425e
hmkarthik37 <hmkarthik37@gmail.com>
2026-06-15 14:56:51 +0530

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,4 @@
 import express from "express";
-import User from "../models/User.js";
-import Order from "../models/Order.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 import {
